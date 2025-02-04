aa5626aa48b833990214e381aadb573b0f7b6e04
Antonellagon16 <526AgonzalezMaurera@frhsd.com>
2025-02-04 10:59:24 +0000

    done

--- script.js
+++ script.js
@@ -1,52 +1,72 @@
+let userScore=0
+let compScore=0
+let userName
+let userChoice
+let roundResult
 
-const getComputerChoice = () => {
-    const randomNumber = Math.floor(Math.random() * 3);
-    switch (randomNumber) {
-        case 0:
-            return 'rock';
-        case 1:
-            return 'paper';
-        case 2:
-            return 'scissors'
-    }
-};
+function start (){
+    //get the name from the form
+    let name = document.getElementById('userName').value;
+    document.getElementById('responseText').innerText = 'Welcome and good luck, ' + name
+        document.getElementById('nameInput').classList.add('d-none');
 
-const determineWinner = (userChoice, computerChoice) => {
-    if (userChoice === computerChoice) {
-        return 'This game is a tie!';
-    }
-    if (userChoice === 'rock') {
-        if (computerChoice === 'paper') {
-            return "Sorry, computer won!";
-        } else {
-            return 'Congratulations you won!'
-        }
-    }
-    if (userChoice === 'paper') {
-        if (computerChoice === 'scissors') {
-            return 'Sorry, computer won!';
-        } else {
-            return "Congratulations, you won!";
-        }
-    }
-    if (userChoice === 'scissors') {
-        if (computerChoice === 'rock') {
-            return 'Sorry, computer won!';
-        } else {
-            return "Congratulations, you won!;";
-        }
-    }
-    if (userChoice === 'bomb') {
-        return "Congratulations, you won!";
-    }
-};
-
-const playGame = (userChoice) => {
-    const computerChoice = getComputerChoice();
-    console.log('You threw:' + userChoice);
-    console.log('The computer threw: ' + computerChoice);
-
-    console.log(determineWinner(userChoice, computerChoice))
-};
+}
 
 
+//getting your choice and the computer choice
+function playGame(userSelection) {
+    const choices = ['rock', 'paper', 'scissors'];
+    const computerSelection = choices[randomNumber = Math.floor(Math.random() * 3)];
+    //the possible random choices:
+    if (randomNumber === 0){
+        compChoice = 'Rock'
+        document.getElementById('compChoice').innerText = '🪨 Rock 🪨';
+    }else if (randomNumber === 1){
+        compChoice = 'Paper'
+        document.getElementById('compChoice').innerText = '📄 Paper 📄';
+    }else{
+        compChoice = 'Scissors'
+        document.getElementById('compChoice').innerText = '✂ Scissor ✂';
+    }
+
+    userChoice = userSelection
+    compChoice = computerSelection
+    
+    //who wins cases
+    if (userSelection === computerSelection){
+        roundResult = "It is a tie!"
+    }else if ((userSelection === 'rock' && computerSelection === 'scissors') || (userSelection === 'paper' && computerSelection === 'rock') || (userSelection === 'scissors' && computerSelection === 'paper')){
+        userScore++;
+        roundResult = "You win!!"
+
+        updateDisplayInfo()
+        isGameOver()
+    }else {
+        compScore++;
+        roundResult = "You lose!"
+
+        updateDisplayInfo()
+        isGameOver()
+    }
+  }
+
+  function isGameOver(){
+    if (userScore === 3){
+        console.log("You Win")
+        document.getElementById('result').innerText = `The game is over. ${userName} won with a final score of ${userScore} to ${compScore}.`
+    } else if (compScore === 3){
+        console.log("Computer Wins")
+        document.getElementById('result').innerText = `The game is over. The computer has won with a final score of ${compScore} to ${userScore}.`
+    }
+  }
+
+  function updateDisplayInfo(){
+    document.getElementById('userScore').innerText = userScore;
+    document.getElementById('compScore').innerText = compScore;
+    document.getElementById('result').innerText = `You: ${userChoice}, Computer: ${compChoice} ${roundResult}`;
+  }
+
+  //button to reload page
+  function reload(){
+    location.reload()
+  }
